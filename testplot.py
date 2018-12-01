7c511243ebeb3344bd8998efcef34bec92c70d39
Hunter Black <huntergblack@gmail.com>
2018-12-01 12:05:22 -0600

    adding test plot

---
+++ testplot.py
@@ -0,0 +1,30 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import random
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+xs = []
+ys = []
+
+def animate(i, xs, ys):
+    xs.append(dt.datetime.now().strftime('%H:%M:%S'))
+    ys.append(random.randint(-10, 10))
+
+    # Limit x and y lists to 20 times
+    xs = xs[-20:]
+    ys = ys[-20:]
+
+    # Draw x and y lists
+    ax.clear()
+    ax.plot(xs, ys)
+
+    # Format plot
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.3)
+    plt.title('Testing dynamic plotting')
+    plt.ylabel('New point')
+
+ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=250)
+plt.show()
